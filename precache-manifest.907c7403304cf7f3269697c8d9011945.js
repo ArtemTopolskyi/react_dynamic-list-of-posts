1837e11bc866921e2c1bb0ecffd4c28f4a546183
Artem Topolskyi <topolskyiav@gmail.com>
2021-03-29 17:56:58 +0300

    Updates

---
+++ precache-manifest.907c7403304cf7f3269697c8d9011945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d349f8dae3ac87eefc98dd36c62f073",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55ccaed22169b5320ef9",
+    "url": "./static/css/main.9bbeda67.chunk.css"
+  },
+  {
+    "revision": "5c5c07bf509dff7e9e26",
+    "url": "./static/js/2.0f788c48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0f788c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ccaed22169b5320ef9",
+    "url": "./static/js/main.147351a7.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
